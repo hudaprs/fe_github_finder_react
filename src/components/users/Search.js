b215e5613cc40dbe4065e255da5f910e399b0d05
Huda Prasetyo <huda.prs2002@gmail.com>
2020-05-12 21:04:47 +0700

    add event and search component

---
+++ src/components/users/Search.js
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+
+class Search extends Component {
+  state = {
+    text: "",
+  };
+
+  onSubmit = (e) => {
+    e.preventDefault();
+    console.log(this.state.text);
+  };
+
+  onChange = (e) => this.setState({ [e.target.name]: e.target.value });
+
+  render() {
+    return (
+      <div>
+        <form className="form" onSubmit={this.onSubmit}>
+          <input
+            type="text"
+            name="text"
+            placeholder="Search User..."
+            value={this.state.text}
+            onChange={this.onChange}
+          />
+          <input
+            type="submit"
+            className="btn btn-dark btn-block"
+            value="Search"
+          />
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Search;
