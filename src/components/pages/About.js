e2f7528de0a442cda0a1a3ef21d04dfd318f4571
Huda Prasetyo <huda.prs2002@gmail.com>
2020-05-13 06:58:40 +0700

    add router

---
+++ src/components/pages/About.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from "react";
+
+const About = () => {
+  return (
+    <Fragment>
+      <h1>About Page</h1>
+      <p>React GitHub Finder</p>
+      <p>v1.0.0</p>
+    </Fragment>
+  );
+};
+
+export default About;
